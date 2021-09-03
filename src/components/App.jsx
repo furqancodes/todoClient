2a66b1583a0dcef87f215efdf4e829eaa1c9dad0
DESKTOP-SH6NK5D\Administrator1 <furqan_ashraf@outlook.com>
2021-09-03 12:53:21 +0500

    input component setup

---
+++ src/components/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import Input from "./Input";
+const App=()=>{
+return(
+    <div className="ui container">
+       <Input/>
+    </div>
+)
+}
+export default App;
---
+++ src/components/Input.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+const Input = () => {
+  return (
+    <div class="ui small input centered grid" style={{marginTop:"2rem"}}>
+      <input type="text" placeholder="Search..."></input>
+      <button class="ui teal button">add</button>
+    </div>
+  );
+};
+export default Input;
